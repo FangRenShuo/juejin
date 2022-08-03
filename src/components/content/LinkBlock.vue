b705fc5a68e24e03f0b57a65962c513a8b24ac80
Dklns <d.klns.245@gmail.com>
2022-08-03 11:56:28 +0800

    构建大致框架

---
+++ src/components/content/LinkBlock.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="sidebar">
+        <p>LinkBlock</p>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+#sidebar {
+    width: 240px;
+    margin-bottom: 16px;
+    background-color: #fff;
+    height: 180px;
+}
+</style>
---
+++ src/components/content/SigninTip.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="signin-tip">
+        <p>signin-tip</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+#signin-tip {
+    width: 240px;
+    height: 96px;
+    margin-bottom: 16px;
+    background-color: #fff;
+}
+</style>
